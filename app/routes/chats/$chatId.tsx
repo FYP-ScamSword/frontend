d7ff33a808befb4a17c3277baad63ce9f4172cf1
claire <claire.xw205@gmail.com>
2023-04-04 15:59:36 +0800

    fix post messages

--- app/routes/chats/$chatId.tsx
+++ app/routes/chats/$chatId.tsx
@@ -8,7 +8,6 @@ import {
   Avatar,
   Input,
   InputGroup,
-  InputLeftAddon,
   InputRightAddon,
   Menu,
   MenuButton,
@@ -18,17 +17,16 @@ import {
 } from "@chakra-ui/react";
 import { ArrowForwardIcon, QuestionOutlineIcon } from "@chakra-ui/icons";
 import { Form, useLoaderData } from "@remix-run/react";
-import Message from "~/server/models/Message";
-import MessageGroup from "~/server/models/MessageGroup";
+import type Message from "~/server/models/Message";
+import type MessageGroup from "~/server/models/MessageGroup";
 import {
   fetchSuggestedResponses,
   retrieveMessages,
   sendMessage,
 } from "~/server/scamchat.server";
-import { ActionFunction, json, type LoaderArgs } from "@remix-run/node";
+import { type ActionFunction, json, type LoaderArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
-import { getSession } from "~/session.server";
-import { LegacyRef, useEffect, useRef } from "react";
+import { type LegacyRef, useRef, useEffect } from "react";
 
 export const loader = async ({ params }: LoaderArgs) => {
   invariant(params.chatId, `params.chatId is required`);
@@ -54,24 +52,33 @@ export const loader = async ({ params }: LoaderArgs) => {
 
 export const action: ActionFunction = async ({ request, params }) => {
   const formData = await request.formData();
-
   const message = String(formData.get("message"));
   if (message.length > 0) {
-    sendMessage("+6584355906", params.chatId!, message);
+    await sendMessage("+6584355906", params.chatId!, message);
   }
   return null;
 };
 export default function ChatDetail() {
   const { messagesGroup, messagesGroupError, suggestedResponses } =
     useLoaderData<typeof loader>();
+
   const messagesEndRef: LegacyRef<HTMLDivElement> = useRef(null);
   const scrollToBottom = () => {
-    messagesEndRef.current!.scrollIntoView({ behavior: "smooth" });
+    messagesEndRef.current!.scrollIntoView({ block: "end", inline: "nearest" });
   };
 
   useEffect(() => {
     scrollToBottom();
   }, [messagesGroup]);
+  
+  if (messagesGroupError) {
+    return (
+      <Text color="red">
+        Error: screenshot cannot be retrieved{" "}
+        {JSON.stringify(messagesGroupError)}
+      </Text>
+    );
+  }
   return (
     <div>
       <Box bg="white">
@@ -104,47 +111,49 @@ export default function ChatDetail() {
                   </Text>
                 </Box>
               </Center>
-              {messageGroup.messages.map((message: Message) =>
-                message.type == 0 ? (
-                  <Flex pt="5" className="receiveMsg" key={message.msg_id}>
-                    <Avatar
-                      className="scammerAvatar"
-                      mr="5"
-                      src="https://bit.ly/dan-abramov"
-                    />
-                    <Box bg="#F2F2F7" p="3" w="300px" borderRadius="10">
-                      <Text>{message.text}</Text>
-                      <Flex pt="1">
-                        <Spacer />
-                        <Text fontSize="xs">{message.time}</Text>
-                      </Flex>
-                    </Box>
-                  </Flex>
-                ) : (
-                  <Flex pt="5" className="sendMsg" key={message.msg_id}>
-                    <Spacer />
-                    <Box bg="#007AFF" p="3" w="300px" borderRadius="10">
-                      <Text color="white">{message.text}</Text>
-                      <Flex pt="1">
-                        <Spacer />
-                        <Text fontSize="xs" color="white">
-                          {message.time}
-                        </Text>
-                      </Flex>
-                    </Box>
-                    <Avatar
-                      className="userAvatar"
-                      ml="5"
-                      src="https://bit.ly/ryan-florence"
-                    />
-                  </Flex>
-                )
-              )}
+              {messageGroup.messages
+                .sort((a, b) => (a.msg_id < b.msg_id ? -1 : 1))
+                .map((message: Message) =>
+                  message.type == 0 ? (
+                    <Flex pt="5" className="receiveMsg" key={message.msg_id}>
+                      <Avatar
+                        className="scammerAvatar"
+                        mr="5"
+                        src="https://bit.ly/dan-abramov"
+                      />
+                      <Box bg="#F2F2F7" p="3" w="300px" borderRadius="10">
+                        <Text>{message.text}</Text>
+                        <Flex pt="1">
+                          <Spacer />
+                          <Text fontSize="xs">{message.time}</Text>
+                        </Flex>
+                      </Box>
+                    </Flex>
+                  ) : (
+                    <Flex pt="5" className="sendMsg" key={message.msg_id}>
+                      <Spacer />
+                      <Box bg="#007AFF" p="3" w="300px" borderRadius="10">
+                        <Text color="white">{message.text}</Text>
+                        <Flex pt="1">
+                          <Spacer />
+                          <Text fontSize="xs" color="white">
+                            {message.time}
+                          </Text>
+                        </Flex>
+                      </Box>
+                      <Avatar
+                        className="userAvatar"
+                        ml="5"
+                        src="https://bit.ly/ryan-florence"
+                      />
+                    </Flex>
+                  )
+                )}
             </div>
           ))}
-          <div ref={messagesEndRef}></div>
+          <Box ref={messagesEndRef} h="4"></Box>
         </Box>
-        <Form method="post" id="msgForm">
+        <Form method="post" id="msgForm" key={Math.random()}>
           <Box
             w="50%"
             h="48px"
